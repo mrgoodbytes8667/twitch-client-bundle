1d46b6c0c18db105da06cbc2a44c6c0e633d1387
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-11-15 14:43:47 -0600

    Remove webhook voter (#27)

--- Tests/Security/TwitchHubSignatureVoterTest.php
+++ Tests/Security/TwitchHubSignatureVoterTest.php
@@ -39,26 +39,7 @@ class TwitchHubSignatureVoterTest extends TestCase
      */
     protected function getVoter(string $signatureClass, bool $has = true)
     {
-        $locator = $this->getMockBuilder(Locator::class)->disableOriginalConstructor()->getMock();
-        $locator->method('get')
-            ->willReturn(new $signatureClass('abc123'));
-        $locator->method('has')
-            ->willReturn($has);
-        return new TwitchHubSignatureVoter($locator);
-    }
-
-    /**
-     *
-     */
-    public function testVoteWebhook()
-    {
-        $voter = $this->getVoter(WebhookSignature::class);
-
-        $this->assertEquals(VoterInterface::ACCESS_DENIED,
-            $voter->vote(
-                $this->getMockBuilder(TokenInterface::class)->getMock(),
-                new Request(),
-                [TwitchHubSignatureVoter::ATTRIBUTE_WEBHOOK]));
+        return new TwitchHubSignatureVoter(new EventSubSignature('abc123'));
     }
 
     /**
@@ -100,20 +81,6 @@ class TwitchHubSignatureVoterTest extends TestCase
             $voter->vote(
                 $this->getMockBuilder(TokenInterface::class)->getMock(),
                 new stdClass(),
-                [TwitchHubSignatureVoter::ATTRIBUTE_EVENTSUB]));
-    }
-
-    /**
-     *
-     */
-    public function testVoteMissingLocator()
-    {
-        $voter = $this->getVoter(EventSubSignature::class, has: false);
-
-        $this->assertEquals(VoterInterface::ACCESS_ABSTAIN,
-            $voter->vote(
-                $this->getMockBuilder(TokenInterface::class)->getMock(),
-                new Request(),
                 [TwitchHubSignatureVoter::ATTRIBUTE_EVENTSUB]));
     }
 }
