06e00bb9193dd937e7e846933d24f83567d6faab
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-04-28 11:25:26 -0500

    Deprecate TwitchImageResize

--- src/Objects/TwitchImageResize.php
+++ src/Objects/TwitchImageResize.php
@@ -4,10 +4,13 @@
 namespace Bytes\TwitchClientBundle\Objects;
 
 
+use JetBrains\PhpStorm\Deprecated;
+
 /**
  * Class TwitchImageResize
  * @package Bytes\TwitchClientBundle\Objects
  */
+#[Deprecated('Since mrgoodbytes8667/twitch-client-bundle v0.7.2, this class is moving to mrgoodbytes8667/twitch-response-bundle.', '\Bytes\TwitchResponseBundle\Objects\ImageResize')]
 class TwitchImageResize
 {
     const WIDTH_THUMBNAIL = 50;
@@ -26,21 +29,34 @@ class TwitchImageResize
      *
      * @return string
      */
+    #[Deprecated('Since mrgoodbytes8667/twitch-client-bundle v0.7.2, this class is moving to mrgoodbytes8667/twitch-response-bundle.', '\Bytes\TwitchResponseBundle\Objects\ImageResize::%name%(%parametersList%)')]
     public static function thumbnail(string $url, int $width = null, int $height = null)
     {
-        if(empty($url))
-        {
+        if (empty($url)) {
             return '';
         }
-        if(empty($width))
-        {
+        if (empty($width)) {
             $width = static::WIDTH_THUMBNAIL;
         }
-        if(empty($height))
-        {
+        if (empty($height)) {
             $height = static::HEIGHT_THUMBNAIL;
         }
         return static::resize($url, $width, $height);
+    }
+
+    /**
+     * @param string $url
+     * @param int $width
+     * @param int $height
+     * @return string
+     */
+    #[Deprecated('Since mrgoodbytes8667/twitch-client-bundle v0.7.2, this class is moving to mrgoodbytes8667/twitch-response-bundle.', '\Bytes\TwitchResponseBundle\Objects\ImageResize::%name%(%parametersList%)')]
+    public static function resize(string $url, int $width, int $height)
+    {
+        if (empty($url)) {
+            return '';
+        }
+        return str_replace('{height}', $height, str_replace('{width}', $width, $url ?? ''));
     }
 
     /**
@@ -50,18 +66,16 @@ class TwitchImageResize
      *
      * @return string
      */
+    #[Deprecated('Since mrgoodbytes8667/twitch-client-bundle v0.7.2, this class is moving to mrgoodbytes8667/twitch-response-bundle.', '\Bytes\TwitchResponseBundle\Objects\ImageResize::%name%(%parametersList%)')]
     public static function twitchGameThumbnail(string $url, int $width = null, int $height = null)
     {
-        if(empty($url))
-        {
+        if (empty($url)) {
             return '';
         }
-        if(empty($width))
-        {
+        if (empty($width)) {
             $width = static::WIDTH_TWITCH_GAME_THUMBNAIL;
         }
-        if(empty($height))
-        {
+        if (empty($height)) {
             $height = static::HEIGHT_TWITCH_GAME_THUMBNAIL;
         }
         return static::resize($url, $width, $height);
@@ -74,35 +88,18 @@ class TwitchImageResize
      *
      * @return string
      */
+    #[Deprecated('Since mrgoodbytes8667/twitch-client-bundle v0.7.2, this class is moving to mrgoodbytes8667/twitch-response-bundle.', '\Bytes\TwitchResponseBundle\Objects\ImageResize::%name%(%parametersList%)')]
     public static function sixteenByNine(string $url, int $width = null, int $height = null)
     {
-        if(empty($url))
-        {
+        if (empty($url)) {
             return '';
         }
-        if(empty($width))
-        {
+        if (empty($width)) {
             $width = static::WIDTH_169;
         }
-        if(empty($height))
-        {
+        if (empty($height)) {
             $height = static::HEIGHT_169;
         }
         return static::resize($url, $width, $height);
-    }
-
-    /**
-     * @param string $url
-     * @param int $width
-     * @param int $height
-     * @return string
-     */
-    public static function resize(string $url, int $width, int $height)
-    {
-        if(empty($url))
-        {
-            return '';
-        }
-        return str_replace('{height}', $height, str_replace('{width}', $width, $url ?? ''));
     }
 }
