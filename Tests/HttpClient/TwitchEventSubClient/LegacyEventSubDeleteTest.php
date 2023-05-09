aad3fecb0f3d700409569d3928709aacbe0b349c
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2023-05-09 11:06:39 -0500

    Revert "Remove deprecated functions (#23)" (#41)
    * This reverts commit a0ca6cfa394f186289c9c573a8719923fa580af7.
    
    * Require test-common ^0.3 | ^0.4

---
+++ Tests/HttpClient/TwitchEventSubClient/LegacyEventSubDeleteTest.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Bytes\TwitchClientBundle\Tests\HttpClient\TwitchEventSubClient;
+
+
+use Bytes\Common\Faker\Twitch\TestTwitchFakerTrait;
+use Bytes\TwitchClientBundle\Event\EventSubSubscriptionDeleteEvent;
+use Bytes\TwitchClientBundle\Tests\MockHttpClient\MockClient;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Response as Http;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+/**
+ *
+ */
+class LegacyEventSubDeleteTest extends TestTwitchEventSubClientCase
+{
+    use TestTwitchFakerTrait;
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testEventSubDeleteClientSuccess()
+    {
+        $client = $this->setupClient(MockClient::empty());
+
+        $cmd = $client->eventSubDelete($this->faker->uuid());
+
+        $this->assertResponseIsSuccessful($cmd);
+        $this->assertResponseStatusCodeSame($cmd, Http::HTTP_NO_CONTENT);
+
+        return $cmd;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testEventSubDeleteResponseCallback()
+    {
+        $event = EventSubSubscriptionDeleteEvent::make($this->faker->accessToken());
+        $dispatcher = $this->createMock(EventDispatcher::class);
+
+        $dispatcher->expects($this->once())
+            ->method('dispatch')
+            ->willReturn($event);
+
+        $client = $this->setupClient(MockClient::empty(), $dispatcher);
+
+        $client->eventSubDelete($this->faker->uuid())->onSuccessCallback();
+    }
+}
